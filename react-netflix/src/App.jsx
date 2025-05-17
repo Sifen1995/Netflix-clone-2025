521160c463d5a0f134f99e5dc5aaea24b6101078
sifen getachew <sifenmesfin10@gmail.com>
2025-05-17 00:11:27 -0700

    banner

--- react-netflix/src/App.jsx
+++ react-netflix/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './pages/home/home';
+
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
       <Home/>
+      
     </div>
   )
 }
---
+++ react-netflix/src/components/banner/banner.jsx
@@ -0,0 +1,55 @@
+import requests from '../../utils/requests';
+import axios from '../../utils/Axios';
+import { useEffect, useState } from 'react';
+import './banner.css'
+
+function Banner  ()  {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        console.log(request);
+
+        setMovie(
+          request.data.results[
+            Math.floor(Math.random() * request.data.results.length)
+          ]
+        );
+      } catch (error) {
+        console.log("error", error);
+      }
+    })();
+  }, []);
+
+  function truncate(str,n) {
+      return str?.length>n? str.substr(0,n-1)+"...":str
+  }
+
+  return (
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
+        backgroundPosition: "center",
+      }}
+    >
+      <div className='banner__contents'>
+        <h1 className='banner_title'>
+            {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div>
+            <button className='banner_button play'>play</button>
+            <button className='banner_button'>My List</button>
+        </div>
+        <div className='banner__description'>
+             {truncate(movie.overview,150)}
+        </div>
+        </div> 
+    </div>
+  );
+};
+
+export default Banner;
